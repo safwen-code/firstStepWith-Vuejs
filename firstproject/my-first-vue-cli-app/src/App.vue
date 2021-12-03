d25abe47c38a6d3421276d58f4679463b8eabf28
safwen-code <safwendjebbi1234@gmail.com>
2021-12-03 17:04:27 +0100

    use my test in this project

--- firstproject/my-first-vue-cli-app/src/App.vue
+++ firstproject/my-first-vue-cli-app/src/App.vue
@@ -1,17 +1,74 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id=" app">
+    <h1>{{ restaurantName }}</h1>
+    <h5>{{ email }}</h5>
+  </div>
+  <MenuList
+    v-for="item in simpleMenu"
+    :key="item.name"
+    :name="item.name"
+    :image="item.image"
+    :inStock="item.inStock"
+    :quantity="item.quantity"
+    :addtoshopping="addtoshopping"
+    :shoppingcart="shoppingcart"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import MenuList from "./components/MenuList.vue";
+import croissant from "./assets/croissant.jpg"
+import eclair from './assets/eclair.jpg'
+import baguette from './assets/baguette.jpeg'
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MenuList,
+  },
+  data() {
+    return {
+      address: "18 avenue du Beurre, Paris, France",
+      email: "hello@cafewithavue.bakery",
+      phone: "01 88 88 88 88",
+      restaurantName: "La belle vue",
+      simpleMenu: [
+        {
+          name: "Croissant",
+          image: {
+            source: croissant,
+            alt: "Un croissant",
+          },
+          inStock: true,
+          quantity: 12,
+        },
+        {
+          name: "Baguette de pain",
+          image: {
+            source: baguette,
+            alt: "Quatre baguettes de pain",
+          },
+          inStock: true,
+          quantity: 5,
+        },
+        {
+          name: "Éclair",
+          image: {
+            source: eclair,
+            alt: "Éclair au chocolat",
+          },
+          inStock: false,
+          quantity: 10,
+        },
+      ],
+      shoppingcart: 0,
+    };
+  },
+  methods: {
+        addtoshopping(quantity) {
+          this.shoppingcart += quantity;
+          console.log(this.shoppingcart);
+        },
+}}
 </script>
 
 <style>
---
+++ firstproject/my-first-vue-cli-app/src/components/MenuList.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  name: "MenuList",
+  props: [
+    "name",
+    "image",
+    "inStock",
+    "quantity",
+    "addtoshopping",
+    "shoppingcart",
+  ],
+};
+</script>
+
+<template>
+  <div class="hello">
+    <div class="menu-item">
+      <h2>shoppingcart {{ shoppingcart }}</h2>
+      <img class="menu-item__image" :src="image.source" :alt="image.alt" />
+      <div>
+        <h3>{{ name }}</h3>
+        <p v-if="inStock">En stock</p>
+        <p v-else>En rupture de stock</p>
+        <div>
+          <label for="add-item-quantity">Quantité : {{ quantity }}</label>
+          <input v:model-number="quantity" type="number" />
+          <button type="button" v-on:click="addtoshopping(quantity)">add to cart</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
