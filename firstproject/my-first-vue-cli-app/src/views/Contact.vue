6da8fa134fe78178c1fefb545d89407733956c7f
safwen-code <safwendjebbi1234@gmail.com>
2021-12-03 18:08:14 +0100

    create a contact page

---
+++ firstproject/my-first-vue-cli-app/src/views/Contact.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  name: "Contact",
+  data() {
+    return {
+      address: "18 avenue du Beurre, Paris, France",
+      email: "hello@cafewithavue.bakery",
+      phone: "01 88 88 88 88",
+    };
+  },
+  computed: {},
+  methods: {},
+};
+</script>
+<template>
+  <h2>Contact</h2>
+  <div>
+    <p>📧 {{ email }}</p>
+    <span>🏠{{ address }}</span> ||
+    <span>📱{{ phone }}</span>
+  </div>
+</template>
