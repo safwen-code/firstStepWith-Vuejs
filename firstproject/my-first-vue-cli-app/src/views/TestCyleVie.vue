2029d90546b6537cca3e9b04d88e7831a4e67cf2
safwen-code <safwendjebbi1234@gmail.com>
2021-12-03 22:56:26 +0100

    life cycle methode

---
+++ firstproject/my-first-vue-cli-app/src/views/TestCyleVie.vue
@@ -0,0 +1,33 @@
+<script>
+export default {
+  name: "TestCyleVie",
+  data() {
+    return {
+      msg: "hello",
+    
+    };
+  },
+    beforeCreate() {
+        console.log("Je ne suis pas encore  créé");
+      },
+      created() {
+        console.log("Je suis créé !");
+      },
+      beforeMount() {
+        console.log("Je vais bientôt être monté sur le DOM!");
+      },
+      mounted() {
+        console.log("Je suis monté sur le DOM!");
+      },
+      beforeUnmounted() {
+        console.log("Je suis sur le point de disparaître du DOM !");
+      },
+      unmounted() {
+        console.log("Je suis supprimé");
+      },
+};
+</script>
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+<style></style>
