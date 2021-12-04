58c1ebf5d73e2f5755ef7fda7f070157f80b3f8d
safwen-code <safwendjebbi1234@gmail.com>
2021-12-04 21:38:26 +0100

    create types to props

--- firstproject/my-first-vue-cli-app/src/components/MenuList.vue
+++ firstproject/my-first-vue-cli-app/src/components/MenuList.vue
@@ -1,50 +1,60 @@
 <script>
 export default {
   name: "MenuList",
-  props: [
-    "name",
-    "image",
-    "inStock",
-    "quantity",
-    "addtoshopping",
-    "shoppingcart",
-    "price",
-  ],
+  props: {
+    name: {
+      type: String,
+    },
+    image: {
+      type: Object,
+    },
+    inStock: {
+      type: Boolean,
+    },
+    quantity: { type: Number, default: 1 },
+    addtoshopping: { type: Function },
+    shoppingcart: { type: Number },
+    price: {
+      type: Number,
+    },
+  },
+
   data() {
-		return {
-			onSale: false
-		}
-	},
-	computed: {
-		generatedPrice() {
-			if (this.onSale) {
-				return (this.price * 0.9).toFixed(2)
-			} else {
-				return this.price
-			}
-		}
-	},
-	beforeMount() {
-		const today = new Date().getDate()
-		if (today % 2 === 1) {
-			this.onSale = true
-		}
-	}
+    return {
+      onSale: false,
+    };
+  },
+  computed: {
+    generatedPrice() {
+      if (this.onSale) {
+        return (this.price * 0.9).toFixed(2);
+      } else {
+        return this.price;
+      }
+    },
+  },
+  beforeMount() {
+    const today = new Date().getDate();
+    if (today % 2 === 1) {
+      this.onSale = true;
+    }
+  },
 };
 </script>
 
 <template>
   <div class="hello">
     <div class="menu-item">
-      <h2>shoppingcart {{ shoppingcart }}</h2>
       <img class="menu-item__image" :src="image.source" :alt="image.alt" />
       <div>
         <h3>{{ name }}</h3>
         <p v-if="inStock">En stock</p>
         <p v-else>En rupture de stock</p>
-        <div>
+        <div class="priceInfo">
           <label>price {{ generatedPrice }}</label>
           <label for="add-item-quantity">Quantité : {{ quantity }}</label>
+        </div>
+        <div class="quantityInfo">
           <input v:model-number="quantity" type="number" />
           <button type="button" v-on:click="addtoshopping(quantity)">
             add to cart
@@ -55,4 +65,19 @@ export default {
   </div>
 </template>
 
-<style></style>
+<style>
+.priceInfo {
+  margin: 10px 50px 20px 0;
+  display: flex;
+  justify-content: space-evenly;
+}
+.quantityInfo {
+  margin: 10px 50px 20px 0;
+  display: flex;
+  justify-content: space-evenly;
+  border: solid;
+}
+.menu-item__image {
+  height: 50px;
+}
+</style>
