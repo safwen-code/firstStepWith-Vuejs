28cd301b56d9f5117d668a1cf31e140b6f4194c2
safwen-code <safwendjebbi1234@gmail.com>
2021-12-05 14:40:30 +0100

    add some mutation & actions

--- firstproject/my-first-vue-cli-app/src/store/index.js
+++ firstproject/my-first-vue-cli-app/src/store/index.js
@@ -1,5 +1,4 @@
-import { createStore } from 'vuex'
-
+import { createStore } from "vuex";
 
 import croissant from "../assets/croissant.jpg";
 import eclair from "../assets/eclair.jpg";
@@ -7,48 +6,53 @@ import baguette from "../assets/baguette.jpeg";
 export default createStore({
   state: {
     address: "18 avenue du Beurre, Paris, France",
-      email: "hello@cafewithavue.bakery",
-      phone: "01 88 88 88 88",
-      restaurantName: "La belle vue",
-      simpleMenu: [
-        {
-          name: "Croissant",
-          image: {
-            source: croissant,
-            alt: "Un croissant",
-          },
-          inStock: true,
-          quantity: 12,
-          price: 2.99,
+    email: "hello@cafewithavue.bakery",
+    phone: "01 88 88 88 88",
+    restaurantName: "La belle vue",
+    simpleMenu: [
+      {
+        name: "Croissant",
+        image: {
+          source: croissant,
+          alt: "Un croissant",
         },
-        {
-          name: "Baguette de pain",
-          image: {
-            source: baguette,
-            alt: "Quatre baguettes de pain",
-          },
-          inStock: true,
-          quantity: 5,
-          price: 55,
+        inStock: true,
+        quantity: 12,
+        price: 2.99,
+      },
+      {
+        name: "Baguette de pain",
+        image: {
+          source: baguette,
+          alt: "Quatre baguettes de pain",
         },
-        {
-          name: "Éclair",
-          image: {
-            source: eclair,
-            alt: "Éclair au chocolat",
-          },
-          inStock: false,
-          quantity: 10,
-          price: 10,
+        inStock: true,
+        quantity: 5,
+        price: 55,
+      },
+      {
+        name: "Éclair",
+        image: {
+          source: eclair,
+          alt: "Éclair au chocolat",
         },
-      ],
-      shoppingcart: 0,
+        inStock: false,
+        quantity: 10,
+        price: 10,
+      },
+    ],
+    shoppingcart: 0,
   },
   mutations: {
+    ADD_ITEMS_TO_SHOPPING_CART(state, amount) {
+      state.shoppingCart += amount;
+    },
   },
   actions: {
+    updateShoppingCart({ commit }, amount) {
+      commit("ADD_ITEMS_TO_SHOPPING_CART", amount);
+    },
   },
-  modules: {
-  },
-  getters:{}
-})
+  modules: {},
+  getters: {},
+});
